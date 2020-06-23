20a083b3484a63c656b8b52464da82f8428558b1
youyanntoto@gmail.com <youyanntoto@gmail.com>
2020-06-23 13:15:22 +0900

    Appを修正

---
+++ template/src/screens/SplashScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Image, SafeAreaView, StyleSheet} from 'react-native'
+
+export default function SplashScreen() {
+	return (
+		<SafeAreaView style={styles.root}>
+			<Image style={styles.appIcon} source={require('../assets/logo.png')}/>
+		</SafeAreaView>
+	)
+}
+
+const styles = StyleSheet.create({
+	root: {
+		backgroundColor: '#fbf2ff',
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	appIcon: {
+		resizeMode: 'contain',
+		width: 160,
+		height: 160,
+		borderRadius: 16,
+	}
+})
