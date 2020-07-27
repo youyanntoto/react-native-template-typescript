ab9cbf5fe6d85005ef316bad19a9df11151c6525
youyanntoto@gmail.com <youyanntoto@gmail.com>
2020-07-27 16:55:36 +0900

    SplashScreenをデフォルドで有効化

--- template/App.tsx
+++ template/App.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react'
+import SplashScreen from 'react-native-splash-screen'
+
 import {RootContainer} from './src/navi'
 
 export default function App() {
 	const [initialized, setInitialized] = useState<boolean>(false)
 
 	useEffect(() => {
+		SplashScreen.hide()
 		// TODO アプリの初期化処理を追加
 		// ex. ログイン状態確認、ローカルDBの初期化
 		setTimeout(() => setInitialized(true), 1000)
